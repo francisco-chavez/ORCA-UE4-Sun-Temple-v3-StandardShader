6665cd6c0991c9dfd281ab6652a9e2bd5060649a
Francisco Chavez-Tejeda <francisco.chavez.509@outlook.com>
2019-01-23 15:17:35 -0800

    Adding Lights Import Script
    Started work on creating a lights importer that will go through the fscene file to import any lights found in the fscene.

---
+++ Assets/ORCA for Unity/Scripts/Util/LightsImportWindow_fscene.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEditor;
+using UnityEngine;
+
+
+public class LightsImportWindow_fscene 
+	: EditorWindow
+{
+	[MenuItem("Window/ORCA/Import Lights")]
+	static void Init()
+	{
+		var window = EditorWindow.GetWindow<LightsImportWindow_fscene>();
+		window.Show();
+	}
+}
